b66e940a64b04543df8ba101ccafe88491419f28
johndoehack23-ux <johndoehack23@gmail.com>
2026-08-17 21:00:20 +0800

    Shit sherlock

--- cogs/ping.py
+++ cogs/ping.py
@@ -14,8 +14,13 @@ class PingCog(commands.Cog):
         prefix = stats.get("prefix", ".")
         latency = round(self.bot.latency * 1000)
 
-        embed = discord.Embed(title="Pong", color=0x2f3136)
-        embed.add_field(name=f"Latency: `{latency}ms`", value=f"**{ctx.author}**",inline=True)
+        embed = discord.Embed(title="Pong", color=discord.Color.random())
+        embed.set_thumbnail(url=self.bot.user.display_avatar.url)
+        embed.add_field(name="Latency", value=f"{latency}ms", inline=True)
+        
+        avatar_url = ctx.author.display_avatar.url if ctx.author.display_avatar else None
+        embed.set_footer(text=str(ctx.author), icon_url=avatar_url)
+        
         await ctx.send(embed=embed)
 
     @app_commands.command(name="ping", description="Check the bot's latency and prefix info")
